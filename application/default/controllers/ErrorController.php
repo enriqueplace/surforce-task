365d39d709899969b3cbabc8913b4e156f33227c
ffeeddee <ffeeddee@594fea7c-bd4c-0410-b541-c955dba8f08a>
2008-06-14 04:44:16 +0000

    Agregada la vista para el manejo de Privilegios. Agregado el privilegesAction en el ErrorController.

--- application/default/controllers/ErrorController.php
+++ application/default/controllers/ErrorController.php
@@ -2,20 +2,20 @@
 
 /**
  * ErrorController - The default error controller class
- * 
+ *
  * @author
- * @version 
+ * @version
  */
 
 require_once 'Zend/Controller/Action.php';
 
-class ErrorController extends Ztask_Controller_Action
+class ErrorController extends Ztask_Generic_Controller
 {
 
     /**
-     * This action handles  
+     * This action handles
      *    - Application errors
-     *    - Errors in the controller chain arising from missing 
+     *    - Errors in the controller chain arising from missing
      *      controller classes and/or action methods
      */
     public function errorAction()
@@ -24,17 +24,22 @@ class ErrorController extends Ztask_Controller_Action
         switch ($errors->type) {
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
-                // 404 error -- controller or action not found                
+                // 404 error -- controller or action not found
                 $this->getResponse()->setRawHeader('HTTP/1.1 404 Not Found');
                 $this->view->title = 'HTTP/1.1 404 Not Found';
                 break;
             default:
-                // application error; display error page, but don't change                
+                // application error; display error page, but don't change
                 // status code
                 $this->view->title = 'Application Error';
                 break;
         }
-        
+
         $this->view->message = $errors->exception;
     }
+    public function privilegesAction()
+    {
+    	$this->view->title = 'No tenes privilegios';
+    }
 }
+?>
