e0f8460a019feed6fd1cccfc339eb6f844c50f6a
enriqueplace <enriqueplace@594fea7c-bd4c-0410-b541-c955dba8f08a>
2009-03-18 19:35:43 +0000

    refactoring

--- trunk/library/Ztask/Generic/Controller.php
+++ trunk/library/Ztask/Generic/Controller.php
@@ -1,21 +1,22 @@
 <?php
 abstract class Ztask_Generic_Controller extends Zend_Controller_Action
 {
- 	protected $registry = null;
-    protected $session = null;
-	protected $debug = null;
-	public function init()
-	{
-		parent::init();
-		$this->view->baseUrl = $this->_request->getBaseUrl ();
-	}
-	function preDispatch()
-	{
-		$auth = Zend_Auth::getInstance ();
-		if ($auth->hasIdentity ()) {
-			$this->view->usuarioLogueado = true;
-			$this->view->role = $auth->getIdentity()->role;
-		}
-	}
+    protected $_registry = null;
+    protected $_session = null;
+    protected $_debug = null;
+
+    public function init()
+    {
+        parent::init();
+        $this->view->baseUrl = $this->_request->getBaseUrl ();
+    }
+    public function preDispatch()
+    {
+        $auth = Zend_Auth::getInstance ();
+        
+        if ($auth->hasIdentity ()) {
+            $this->view->usuarioLogueado = true;
+            $this->view->role = $auth->getIdentity()->role;
+        }
+    }
 }
-?>
